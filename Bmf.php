651c7ba412b16bb33476cc7519a71698180c881f
crs2 <crs@centrum.cz>
2020-05-23 22:25:59 +0200

    Update Bmf.php
    save()
    $transparentBackground
    bug fixing
    docs

--- Bmf.php
+++ Bmf.php
@@ -1,45 +1,49 @@
 <?php
 /**
- * This class can load a BMF file and output its character bitmaps.
+ * Use this class to load a BMF files and write with it.
  *
- * @author crs@centrum.cz
+ * @see http://bmf.php5.cz/?page=format
+ * @author <crs@centrum.cz>
  */
 class Bmf {
 
-    /** @var constant in the beginning of the file */
+    /** @var header constant in the beginning of the file */
     const MAGIC_HEADER = "\xE1\xE6\xD5\x1A";
 
-    /** @var int access as file */
+    /** @var int access from/to file */
     const ACCESS_FILE = 0;
 
-    /** @var int access as memory/string */
+    /** @var int access from/to memory */
     const ACCESS_MEMORY = 1;
 
     /** @var int used in textAsImage(), output as transparent PNG */
     const OUTPUT_PNG = 1;
 
-    /** @var int used in textAsImage(), output as transparent HTML <img src="data:..."> */
+    /** @var int used in textAsImage(), output as HTML <img src="data:..."> */
     const OUTPUT_HTML = 2;
 
     /** @var int used in textAsImage(), return image resource */
     const OUTPUT_RESOURCE = 3;
 
-    /** @var bool used in toString(), list palette */
-    const TOSTRING_PALETTE = true;
+    /** @var bool used in toString(), include attributes */
+    const TOSTRING_ATTRIBUTES = 1;
 
-    /** @var bool used in toString(), list chars */
-    const TOSTRING_CHARS = true;
+    /** @var bool used in toString(), include palette */
+    const TOSTRING_PALETTE = 2;
+
+    /** @var bool used in toString(), include chars */
+    const TOSTRING_CHARS = 4;
 
     /** @var int way to access the module */
     public $accessMode = self::ACCESS_FILE;
 
-    /** @var resource|null file handle for reading from disk */
+    /** @var resource|null file handle for file access */
     public $fileHandle;
 
     /** @var int offset for reading from memory */
     public $offset = 0;
 
-    /** @var string font's file name or memory */
+    /** @var string font's file name or raw data */
     public $font;
 
     /** @var int version (0x11 for v1.1) */
@@ -48,16 +52,16 @@ class Bmf {
     /** @var int line height */
     public $lineHeight;
 
-    /** @var int length over the base line in pixels (–128…127) */
+    /** @var int length over the base line in pixels (–128...127) */
     public $sizeOver;
 
-    /** @var int length under the base line in pixels (–128…127) */
+    /** @var int length under the base line in pixels (–128...127) */
     public $sizeUnder;
 
-    /** @var int add space after each char in pixels (–128…127) */
+    /** @var int add space after each char in pixels (–128...127) */
     public $addSpace;
 
-    /** @var int size inner (small letters height) (–128…127) */
+    /** @var int size inner (small letters height) (–128...127) */
     public $sizeInner;
 
     /** @var int count of used colors */
@@ -84,16 +88,21 @@ class Bmf {
     /** @var array color palette as array of r,g,b components (0..63 each) */
     protected $palette;
 
-    /** @var int extra gap between letters when outputting text */
+    /** @var bool keep background transparent? */
+    public $transparentBackground = true;
+
+    /** @var int gap between letters when outputting text (in addition to addSpace) */
     public $letterGap = 0;
 
     /**
-     * Constructor. Load a font if parameter provided.
+     * Constructor. Load a font if a parameter is provided.
      *
-     * @param
+     * @param string|null $font if specified, $this->load() is called
+     * @param int $accessMode access mode, see self::ACCESS_* constants
      */
-    function __construct($font = null)
+    function __construct($font = null, $accessMode = self::ACCESS_FILE)
     {
+        $this->accessMode = $accessMode;
         if (($this->font = $font) !== null) {
             $this->load($font);
         }
@@ -125,9 +134,9 @@ class Bmf {
     }
 
     /**
-     * Closes a font
+     * Closes a font (needed for file access)
      *
-     * @return bool succes status
+     * @return void
      */
     public function close()
     {
@@ -138,45 +147,51 @@ class Bmf {
     }
 
     /**
-     * Allocate font's palette to given image
+     * Allocate font's palette colors to given image
      *
      * @param resource &$image
-     * @return void
+     * @return array palette of allocated font's colors
      */
     public function imagePalette(&$image)
     {
+        $result[0] = imagecolorallocate($image, 0, 0, 0);
         foreach ($this->palette as $key => $value) {
-            $pal[$key] = imagecolorallocate($image, $value[0], $value[1], $value[2]);
+            $result []= imagecolorallocate($image, $value[0], $value[1], $value[2]);
         }
+        return $result;
     }
 
     /**
-     * Output string to given image.
+     * Write a string with the current font to given image
      *
      * @param resource &$image
      * @param int $x x-coordinate
      * @param int $y y-coordinate
-     * @param string $text text to output
-     * @return int width of the text
+     * @param string $string text to output
+     * @return int width of the text written
      */
-    public function imageString(&$image, $x, $y, $text)
+    public function imageString(&$image, $x, $y, $string)
     {
-        for ($i = 0, $x0 = $x; $i < strlen($text); $i++) {
-            $letter = &$this->tablo[ord($text[$i])];
+        $palette = $this->imagePalette($image);
+        for ($i = 0, $x0 = $x; $i < strlen($string); $i++) {
+            $letter = &$this->tablo[ord($string[$i])];
             for ($j = 0, $rely = $letter['rely']; $j < $letter['height']; $j++) { 
                 for ($k = 0; $k < $letter['width']; $k++) {
-                    imagesetpixel($image, $x + $letter['relx'] + $k, $y + $rely + $j, 
-                        ord($letter['data'][$j * $letter['width'] + $k]));
+                    if ($color = ord($letter['data'][$j * $letter['width'] + $k])) {
+                        imagesetpixel($image, $x + $letter['relx'] + $k, $y + $rely + $j, $palette[$color]);
+                    }
                 }
             }
             $x += $letter['shift'] + $this->addSpace + $this->letterGap;
         }
+        return $x - $x0;
     }
 
     /**
      * Load a BMF font
      *
-     * @param string $filename font's file name or memory
+     * @param string $font font's file name or raw data
+     * @param int $accessMode access mode, see self::ACCESS_* constants
      * @return bool success
      */
     public function load($font, $accessMode = self::ACCESS_FILE)
@@ -219,7 +234,7 @@ class Bmf {
                 'height' => $height = ord($buffer[2]),
                 'relx' => unpack('c', $buffer[3])[1],
                 'rely' => unpack('c', $buffer[4])[1],
-                'shift' => unpack('c', $buffer[5])[1],
+                'shift' => ord($buffer[5]),
                 'data' => ''
             ];
             if ($width && $height) {
@@ -233,23 +248,27 @@ class Bmf {
     }
 
     /**
-     * Open a font
+     * Open a font (needed for file access)
      *
+     * @param int $accessMode access mode, see self::ACCESS_* constants
      * @return bool succes status
      */
-    public function open()
+    public function open($accessMode = self::ACCESS_FILE)
     {
+        $this->accessMode = $accessMode;
         if ($this->accessMode == self::ACCESS_FILE) {
             if (!file_exists($this->font)) {
                 return false;
             }
             return $this->fileHandle = fopen($this->font, 'r');
         }
+        return true;
     }
 
     /**
-     * Read next $bytes bytes off a font.
+     * Read next $bytes bytes off a font
      *
+     * @param int $bytes amount of bytes to be read
      * @return string data
      */
     public function read($bytes)
@@ -264,25 +283,53 @@ class Bmf {
     }
 
     /**
+     * Saves (or returns) font data in the format
+     *
+     * @param int $accessMode access mode, see self::ACCESS_* constants
+     * @return bool|string success status for file access, raw data for memory access
+     */
+    public function save($accessMode = self::ACCESS_FILE)
+    {
+        $result = self::MAGIC_HEADER;
+        foreach (explode(' ', 'version lineHeight sizeOver sizeUnder addSpace sizeInner usedColors highestColor') as $i) {
+            $result .= chr($this->{$i});
+        }
+        $result .= pack('L', $this->reserved) . chr($this->colors);
+        for ($i = 0; $i < $this->colors; $i++) {
+            $result .= chr($this->palette[$i][0] / 4) . chr($this->palette[$i][1] / 4) . chr($this->palette[$i][2] / 4);
+        }
+        $result .= chr(strlen($this->title)) . $this->title . pack('S', $this->numChars);
+        for ($i = 0; $i < 256; $i++) {
+            if (($w = $this->tablo[$i]['width']) * ($h = $this->tablo[$i]['height']) || ($shift = $this->tablo[$i]['shift'])) {
+                $result .= chr($i) . chr($w) . chr($h) 
+                    . pack('c', $this->tablo[$i]['relx']) . pack('c', $this->tablo[$i]['rely']) 
+                    . chr($this->tablo[$i]['shift']) . $this->tablo[$i]['data'];
+            }
+        }
+        if ($this->accessMode == self::ACCESS_FILE) {
+            return file_put_contents($this->font, $result);
+        } elseif ($this->accessMode == self::ACCESS_MEMORY) {
+            return $result;
+        }
+    }
+
+    /**
      * Output font's text as image
      *
      * @param string $text
      * @param int $output see self::OUTPUT_* constants
-     * @return void|resource|false according to $output
+     * @return void|resource|string according to $output
      */
     public function textAsImage($text, $output = self::OUTPUT_PNG)
     {
-        for ($i = 0, $width = 0; $i < strlen($text) - 1; $i++) { // determine image width
-            $width += $this->tablo[ord($text[$i])]['shift'] + $this->addSpace;
-        }
-        $image = imagecreate($width + ($x = max(-$this->tablo[ord($text[0])]['relx'], 0)) 
-            + $this->tablo[ord($text[strlen($text) - 1])]['relx'] 
-            + $this->tablo[ord($text[strlen($text) - 1])]['width']
-            + (strlen($text) - 1) * $this->letterGap, 
-            $height = max($this->lineHeight, -$this->sizeOver + $this->sizeUnder));
-        imagealphablending($image, true);
-        imagefilledrectangle($image, 0, 0, $width - 1, $height - 1, imagecolorallocatealpha($image, 0, 0, 0, 127));
+        $width; $height;
+        $this->textSize($text, $width, $height);
+        $image = imagecreate($width, $height);
+        imagealphablending($image, $this->transparentBackground);
+        imagefilledrectangle($image, 0, 0, $width - 1, $height - 1, 
+            imagecolorallocatealpha($image, 0, 0, 0, $this->transparentBackground ? 127 : 0));
         $this->imagePalette($image);
+        $x = max(-$this->tablo[ord($text[0])]['relx'], 0);
         $this->imageString($image, $x, 0, $text);
         switch ($output) {
             case self::OUTPUT_PNG:
@@ -296,7 +343,7 @@ class Bmf {
                 $image_data = ob_get_contents();
                 imagedestroy($image);
                 ob_end_clean(); 
-                $result = '<img src="data:image/png;base64,' . base64_encode($image_data) . '" alt="' . htmlspecialchars($this->toString(), ENT_HTML5, 'UTF-8') . '"/>';
+                $result = '<img src="data:image/png;base64,' . base64_encode($image_data) . '" alt="' . htmlspecialchars($this->toString(0), ENT_HTML5, 'UTF-8') . '"/>';
                 return $result;
             case self::OUTPUT_RESOURCE:
                 return $image;
@@ -307,28 +354,53 @@ class Bmf {
     }
 
     /**
-     * Return font's parameters
+     * Get width and height of text of current font
      *
+     * @param string $text
+     * @param int &$width
+     * @param int &$height
+     * @return void
+     */
+    public function textSize($text, &$width, &$height)
+    {
+        $width = 0;
+        for ($i = 0; $i < strlen($text) - 1; $i++) {
+            $width += $this->tablo[ord($text[$i])]['shift'] + $this->addSpace;
+        }
+        $width += max(-$this->tablo[ord($text[0])]['relx'], 0) 
+            + $this->tablo[ord($text[strlen($text) - 1])]['relx'] 
+            + $this->tablo[ord($text[strlen($text) - 1])]['width']
+            + (strlen($text) - 1) * $this->letterGap; 
+        $height = max($this->lineHeight, -$this->sizeOver + $this->sizeUnder, 1);
+    }
+
+    /**
+     * Return font's parameters in a human-readable form.
+     *
+     * @param int $include see self::TOSTRING_* constants
      * @return string
      */
-    public function toString()
+    public function toString($include = self::TOSTRING_ATTRIBUTES | self::TOSTRING_PALETTE | self::TOSTRING_CHARS)
     {
-        $result = '';
-        foreach (['title', 'colors', 'numChars', 'lineHeight', 'sizeOver', 'sizeUnder', 'addSpace', 'sizeInner', 
-            'usedColors', 'highestColor'] as $i) {
-            $result .= "$i: " . $this->{$i} . "\n";
+        $result = 'title: '  . $this->title . "\n";
+        if ($include & self::TOSTRING_ATTRIBUTES) {
+            foreach (['colors', 'numChars', 'lineHeight', 'sizeOver', 'sizeUnder', 'addSpace', 'sizeInner', 
+                'usedColors', 'highestColor'] as $i) {
+                $result .= "$i: " . $this->{$i} . "\n";
+            }
         }
-        if (self::TOSTRING_PALETTE) {
+        if ($include & self::TOSTRING_PALETTE) {
             $result .= 'palette:';
             for ($i = 0; $i < $this->colors; $i++) {
-                $result .= ' <span style="background:#' . ($color = substr('00000' . dechex(($this->palette[$i][0] << 16) | ($this->palette[$i][1] << 8) | $this->palette[$i][2]), -6)) . '">#</span>' . $color;
+                $color = substr('00000' . dechex(($this->palette[$i][0] << 16) | ($this->palette[$i][1] << 8) | $this->palette[$i][2]), -6);
+                $result .= ' <span style="background:#' . $color . '">#</span>' . $color;
             }
             $result .= "\n";
         }
-        if (self::TOSTRING_CHARS) {
+        if ($include & self::TOSTRING_CHARS) {
             $result .= 'chars: ';
             for ($i = 0; $i < 256; $i++) {
-                $result .= ($this->tablo[$i]['shift'] ? ($i <= 32 ? "#$i " : ($i > 127 ? " #$i" : chr($i))) : '');
+                $result .= ($this->tablo[$i]['shift'] ? ($i <= 32 ? "<small>#$i</small> " : ($i >= 127 ? " <small>#$i</small>" : chr($i))) : '');
             }
             $result .= "\n";        	
         }
